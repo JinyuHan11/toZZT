fb6220b398a7264a00843730ff5a83b02afd950a
hanjinyu <precisenessfree@163.com>
2021-02-03 19:52:12 +0800

    add get_img_block method

--- abnormal_detection/get_block_points.py
+++ abnormal_detection/get_block_points.py
@@ -9,16 +9,19 @@ from config import *
 import cv2
 import json
 
-img = cv2.imread("C:/Users/50106/Desktop/1.jpg")  #获取到ndarray数组,(355,500,3)
-print(img.shape[0])
-new_img = img[100:300,100:300]  #先高，后宽
-print(type(new_img))
-print(new_img.shape)
-test_dict = {}
-test_dict['0_1'] = new_img
-print(test_dict)
+# img = cv2.imread("C:/Users/50106/Desktop/1.jpg")  #获取到ndarray数组,(355,500,3)
+# print(img.shape[0])
+# new_img = img[100:300,100:300]  #先高，后宽
+# print(type(new_img))
+# print(new_img.shape)
+# test_dict = {}
+# test_dict['0_1'] = new_img
+# print(test_dict)
 # cv2.imwrite("C:/Users/50106/Desktop/2.jpg",new_img)
 # cv2.waitKey(0)
+
+
+
 
 
 #从图像中截取BLOCK_SIZE大小的区域，保存为json文件
@@ -40,7 +43,7 @@ def get_img_block(pic_path):
             #返回字典中键值对数量
             print("len of img_block_dict:%s"%len(img_block_dict))
             #将图像块数据保存为json文件,之后使用json.load读取
-            with open(JSON_FILE,'w',encoding='utf-8') as f:
+            with open(BLOCK_JSON_FILE,'w',encoding='utf-8') as f:
                 f.write(json.dumps(img_block_dict,ensure_ascii=False))
 
 #计算各图像块均值点函数并进行保存：①从json文件提取图像块数组
@@ -48,3 +51,15 @@ def get_img_block(pic_path):
 # ③将坐标-点对再存入字典，形式为：{'i_j':[float1,float2,float3]}
 # ④将字典再此存为json文件
 
+def get_block_point(pic_path):
+    get_img_block(pic_path)
+    img_block_dict = {}
+    with open(BLOCK_JSON_FILE,'r',encoding='utf-8') as f:
+        img_block_dict = json.load(f)
+
+    img_block_keys = img_block_dict.keys()
+    img_block_values = img_block_dict.values()
+
+    
+
+    pass
